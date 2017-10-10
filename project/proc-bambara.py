3a85e10c36fa1e8c0ed67408a8bae7504bf916e5
KatyaAplonova <aplooon@gmail.com>
2017-10-10 19:09:57 +0000

    gjhdsfklk

--- project/proc-bambara.py
+++ project/proc-bambara.py
@@ -9,32 +9,32 @@ def get_udtags(lem, pos, gloss, all_glosses):
 #	print(lem, pos, gloss, all_glosses, file=sys.stderr)
 #	part of speech tags
 	if pos == 'adj':
-                tags[0] = 'ADJ'
-        if pos == 'adv':
-                tags[0] = 'ADV'
-        if pos == 'adv.p':
-                tags[0]='ADV'
-        if pos == 'adv.ex':
-                tags[0] = 'ADV'
-        if pos == 'num':
-                tags[0] = 'NUM'
-        if pos == 'n':
-                tags[0] = 'NOUN'
-        if pos == 'n.prop':
-                tags[0] = 'PROPN'
-        if pos == 'v':
-                tags[0] = 'VERB'
-        if pos == 'vq':
-                tags[0] ='VERB'
-        if pos == 'ptcp':
-                tags[0] ='VERB'
-        if pos == 'pers':
-                tags[0] = 'PRON'
-                tags[1].append('PronType=Prs')
-        if pos == 'pron':
-                tags[0] = 'PRON'
-        if pos == 'pm':
-                tags[0] = 'AUX' 
+		tags[0] = 'ADJ'
+	if pos == 'adv':
+		tags[0] = 'ADV'
+	if pos == 'adv.p':
+		tags[0]='ADV'
+	if pos == 'adv.ex':
+		tags[0] = 'ADV'
+	if pos == 'num':
+		tags[0] = 'NUM'
+	if pos == 'n':
+		tags[0] = 'NOUN'
+	if pos == 'n.prop':
+		tags[0] = 'PROPN'
+	if pos == 'v':
+		tags[0] = 'VERB'
+	if pos == 'vq':
+		tags[0] ='VERB'
+	if pos == 'ptcp':
+		tags[0] ='VERB'
+	if pos == 'pers':
+		tags[0] = 'PRON'
+		tags[1].append('PronType=Prs')
+	if pos == 'pron':
+		tags[0] = 'PRON'
+	if pos == 'pm':
+		tags[0] = 'AUX' 
 	if pos == 'conj' and lem in ["bàri", "jàa", "jàga", "kélen", "kòni", "nká", "nk'", "ô", "ôo", "wô", "tàrí", "tɛ̀rí", "wà", "wáa", "w'", "wálà", "wàlámà", "wàlímà", "wáli", "wó"]:
 		tags[0] = 'CCONJ'
 	if pos == 'conj' and lem in ["bárì", "bárìsá", "bárì sá", "báwò", "bɛ́ɛ", "bína", "bíɲa", "dàma", "dɔ́rɔn", "jànkó", "jàngó", "kàsɔrɔ", "kàtugu", "kàtuguni", "kɔ́ntɛ̀", "mînkɛ́", "mînkɛ́ni", "ní", "n'ó tɛ́", "nóntɛ́", "nɔ́ntɛ", "sá", "sábu", "sáabu", "sábi", "sábula", "sàfɛ", "sánì", "sánni", "sán'", "sánn'", "sànkó", "sàngó", "tɛ́sɛ", "tílen", "wálasa", "wáasa", "wálisa", "yáa", "yála", "yálasa", "yáasa", "yála", "yálisa", "jáasa", "yànni", "yànn'", "yàli"]:
@@ -43,82 +43,83 @@ def get_udtags(lem, pos, gloss, all_glosses):
 		tags[0] = 'AUX'
 #	then glosses
 	if gloss == 'ABR':
-                tags[1].append('Abbr=Yes')
-        if gloss == 'REL':
-                tags[1].append('PronType=Rel')
-        if gloss == 'PFV.TR':
-                tags[1].append('Aspect=Perf')
-                tags[1].append('Valency=2')
-				tags[1].append('Polarity=Pos')
-        if gloss == 'SBJV':
-                tags[1].append('Mood=Subj')
-				tags[1].append('Polarity=Aff')
-        if gloss == 'REFL':
-                tags[1].append('Reflexive=Yes')
-        if gloss == 'RECP':
-                tags[1].append('PronType=Rcp')
-		if gloss == 'EQU':
-                tags[1].append('Polarity=Pos')
-        if gloss == 'QUAL.NEG':
-                tags[1].append('Polarity=Neg')
-        if gloss == 'QUAL.AFF':
-                tags[1].append('Polarity=Pos')
-        if gloss == 'PST':
-                tags[1].append('Tense=Past')
-        if gloss == 'PROH':
-                tags[1].append('Polarity=Neg')
-                tags[1].append('Mood=Imp')
-        if gloss == 'PROG.NEG':
-                tags[1].append('Polarity=Neg')
-                tags[1].append('Aspect=Prog')        
+		tags[1].append('Abbr=Yes')
+	if gloss == 'REL':
+		tags[1].append('PronType=Rel')
+	if gloss == 'PFV.TR':
+		tags[1].append('Aspect=Perf')
+		tags[1].append('Valency=2')
+		tags[1].append('Polarity=Pos')
+	if gloss == 'SBJV':
+		tags[1].append('Mood=Subj')
+		tags[1].append('Polarity=Aff')
+	if gloss == 'REFL':
+		tags[1].append('Reflexive=Yes')
+	if gloss == 'RECP':
+		tags[1].append('PronType=Rcp')
+	if gloss == 'EQU':
+		tags[1].append('Polarity=Pos')
+	if gloss == 'QUAL.NEG':
+		tags[1].append('Polarity=Neg')
+	if gloss == 'QUAL.AFF':
+		tags[1].append('Polarity=Pos')
+	if gloss == 'PST':
+		tags[1].append('Tense=Past')
+	if gloss == 'PROH':
+		tags[1].append('Polarity=Neg')
+		tags[1].append('Mood=Imp')
+	if gloss == 'PROG.NEG':
+		tags[1].append('Polarity=Neg')
+		tags[1].append('Aspect=Prog')	
 	if gloss == 'PROG.AFF':
-                tags[1].append('Polarity=Pos')
-                tags[1].append('Aspect=Prog')
+		tags[1].append('Polarity=Pos')
+		tags[1].append('Aspect=Prog')
 	if gloss == 'PFV.EMPH':
-                tags[1].append('Polarity=Pos')
-                tags[1].append('Aspect=Perf')
-        if gloss == 'PFV.NEG':
-                tags[1].append('Polarity=Neg')
-                tags[1].append('Aspect=Perf')
-        if gloss == 'IPFV.NEG':
-                tags[1].append('Polarity=Neg')
-                tags[1].append('Aspect=Imp')
-        if gloss == 'IPFV.AFF':
-                tags[1].append('Polarity=Pos')
-                tags[1].append('Aspect=Imp')
-        if gloss == 'INFR.NEG':
+		tags[1].append('Polarity=Pos')
+		tags[1].append('Aspect=Perf')
+	if gloss == 'PFV.NEG':
+		tags[1].append('Polarity=Neg')
+		tags[1].append('Aspect=Perf')
+	if gloss == 'IPFV.NEG':
+		tags[1].append('Polarity=Neg')
+		tags[1].append('Aspect=Imp')
+	if gloss == 'IPFV.AFF':
+		tags[1].append('Polarity=Pos')
+		tags[1].append('Aspect=Imp')
+	if gloss == 'INFR.NEG':
 		tags[1].append('Evident=Infer')
 		tags[1].append('Aspect=Perf')
-                tags[1].append('Polarity=Neg')
-        if gloss == 'INFR':
-                tags[1].append('Evident=Infer')
+		tags[1].append('Polarity=Neg')
+	if gloss == 'INFR':
+		tags[1].append('Evident=Infer')
 		tags[1].append('Aspect=Perf')
-                tags[1].append('Polarity=Pos')				
-        if gloss == 'IMP':             
+		tags[1].append('Polarity=Pos')				
+	if gloss == 'IMP':	     
 		tags[1].append('Mood=Imp')
 		tags[1].append('Polarity=Pos')	
-        if gloss == 'FUT.NEG':
-                tags[1].append('Polarity=Neg')
-                tags[1].append('Tense=Fut')
-        if gloss == 'FUT':
-                tags[1].append('Tense=Fut')
+	if gloss == 'FUT.NEG':
+		tags[1].append('Polarity=Neg')
+		tags[1].append('Tense=Fut')
+	if gloss == 'FUT':
+		tags[1].append('Tense=Fut')
 		tags[1].append('Polarity=Pos')
 	if gloss == 'CERT':
-                tags[1].append('Tense=Fut')
+		tags[1].append('Tense=Fut')
 		tags[1].append('Polarity=Pos')	
 	if gloss == 'BE':
-                tag[1].append('Polarity=Pos')				
-        if gloss == 'COP.NEG':
-                tags[1].append('Polarity=Neg')
-        if gloss == 'COND.NEG':
-                tags[1].append('Mood=Cnd')
-                tags[1].append('Polarity=Neg')
-        if gloss == 'COND.AFF':
-                tags[1].append('Mood=Cnd')
-                tags[1].append('Polarity=Pos')if gloss == 'PST':
-                tags[1].append('Tense=Past')
+		tags[1].append('Polarity=Pos')				
+	if gloss == 'COP.NEG':
+		tags[1].append('Polarity=Neg')
+	if gloss == 'COND.NEG':
+		tags[1].append('Mood=Cnd')
+		tags[1].append('Polarity=Neg')
+	if gloss == 'COND.AFF':
+		tags[1].append('Mood=Cnd')
+		tags[1].append('Polarity=Pos')
+	if gloss == 'PST':
+		tags[1].append('Tense=Past')
 	if gloss == 'DEM':
-                tags[1].append('PronType=Dem')
+		tags[1].append('PronType=Dem')
 		tags[1].append('Definite-Def')
 	if gloss == '3SG.EMPH':
 		tags[1].append('Number=Sing')
@@ -160,10 +161,10 @@ def get_udtags(lem, pos, gloss, all_glosses):
 		tags[1].append('Person=1')
 #	now mrph
 	if 'DEF' in all_glosses:
-                tags[1].append('Definite=Def')
-        if 'PFV.INTR' in all_glosses:
-                tags[1].append('Aspectpect=Perf')
-                tags[1].append('Valency=1')
+		tags[1].append('Definite=Def')
+	if 'PFV.INTR' in all_glosses:
+		tags[1].append('Aspectpect=Perf')
+		tags[1].append('Valency=1')
 		tags[1].append('Polarity=Pos')
 	if 'PROG' in all_glosses:
 		tags[1].append('Aspect=Prog')
@@ -192,11 +193,11 @@ def get_udtags(lem, pos, gloss, all_glosses):
 	if 'ADJ' in all_glosses:
 		tags[1].append('AdjType=Attr')	
 	if 'ORD' in all_glosses:
-                tags[1].append('NumType=Ord')
+		tags[1].append('NumType=Ord')
 	if 'CARDINAL' in all_glosses:
-                tags[1].append('NumType=Card')
+		tags[1].append('NumType=Card')
 	if 'CAUS' in all_glosses:
-                tags[1].append('Voice=Cau')
+		tags[1].append('Voice=Cau')
 	if tags[1] == []:
 		tags[1] = ['_']
 
