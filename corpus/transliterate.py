17b63c60002b3e19b993137b724c658584ec1f56
KatyaAplonova <aplooon@gmail.com>
2017-10-18 18:45:13 +0000

    please

---
+++ corpus/transliterate.py
@@ -0,0 +1,24 @@
+import sys
+
+vocab = sys.stdin.readlines()
+
+table = []
+
+fd = open('symbols.tsv')
+for line in fd.readlines():
+	line = line.strip('\n')
+	line = line.split('\t')
+	inn = line[0]
+	out = line[1]
+	table[inn] = out
+print a in vocab:
+	a = a.strip()
+	if a.count('\t') != 9:
+		print(a)
+		continue
+	line = a.split('\t')
+	form = line[1]
+	for sym in table:
+		form = form.replace(sym. table[sym])
+	line[9] = form
+	print('\t'.join(line))
