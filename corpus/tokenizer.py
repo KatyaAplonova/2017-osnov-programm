79d8adcd48c42cb0552e10fe84f3be395847e9db
KatyaAplonova <aplooon@gmail.com>
2017-10-19 18:35:14 +0000

    dfghjk

--- corpus/tokenizer.py
+++ corpus/tokenizer.py
@@ -2,20 +2,23 @@ import sys
 sent_id = 1
 lines = sys.stdin.readlines()
 
-for line in lines:	
-	if line.strip() == '':
+for line in lines:
+	line = line.strip()	
+	if line  == '':
 		continue
 	print('# sent_id = :%d' % (sent_id))
-	print('# text = %s' % (line.text.strip()))
+	print('# text = %s' % (line.strip()))
 	tok_id = 1
 	punctuation = ['߸',':','(',')',':','?']
 	for symbols in punctuation:
 #in the text there are french titles and extra new lines, probably, we should ignore french words and delete extra new lines?..	
 		line = line.replace(symbols, ' ' + symbols)
 	tokens = line.split(' ')
+#	print('TOKENS: ', tokens)
 	for token in tokens:
-		print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ((tok_id, token), '_', '_', '_', '_', '_', '_', '_')
+		if token.strip() == '':
+			continue
+		print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (tok_id, token, '_', '_', '_', '_', '_', '_', '_', '_'))
 		tok_id = tok_id + 1
-	count_tok = count_tok + 1
 	print('')
 	sent_id = sent_id + 1
