f809bd25aa6b224068f1b581b9e359cc4c603c9c
KatyaAplonova <aplooon@gmail.com>
2017-10-19 07:59:41 +0000

    werty

--- corpus/tokenizer.py
+++ corpus/tokenizer.py
@@ -1,14 +1,14 @@
 import sys
 
 lines = sys.stdin.readlines()
-print('# sent_id = %s:%d' % (doc_id, sent_id))
-print('# text = %s' % (sent.text.strip()))
+	print('# sent_id = %s:%d' % (doc_id, sent_id))
+	print('# text = %s' % (sent.text.strip()))
 tok_id = 1
-punctuation = ['߸',':']
-        for symbols in punctuation:
-                line = line.replace(symbols, ' ' + symbols)
-print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ((tok_id, token), '_', '_', '_', '_', '_', '_', '_')
-		tok_id = tok_id + 1
-		count_tok = count_tok + 1
-	print('')
+	punctuation = ['߸',':']
+		for symbols in punctuation:
+	line = line.replace(symbols, ' ' + symbols)
+	print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ((tok_id, token), '_', '_', '_', '_', '_', '_', '_')
+tok_id = tok_id + 1
+count_tok = count_tok + 1
+print('')
 sent_id = sent_id + 1
